1ed70e04d5b62077f5349968da29023cbc40c7be
Elijah wines <elijahwines84@yahoo.com>
2020-08-17 12:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
     .then(json => console.log(json));
-  renderBooks(json["names"])
+  renderBooks(json[0]["names"])
 }
 
 function renderBooks(books) {
